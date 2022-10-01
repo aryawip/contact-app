c61f6f49fcfb254b792a04d9e5737684822493d5
Arya Wira P <awp.aryawira@gmail.com>
2022-10-01 20:09:33 +0700

    memodifikasi app kontak dengan yargs

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
+const yargs = require('yargs')
+// const {simpanContact} = require('./contact')
 const contacts = require('./contact')
 
-const main = async () => {
-    const nama = await contacts.pertanyaan('Masukkan nama anda : ')
-    const noHP = await contacts.pertanyaan('Masukkan no HP anda : ')
-    const email = await contacts.pertanyaan('Masukkan email anda : ')
+yargs.command ({
+    command: 'add',
+    describe: 'Menambahkan kontak baru',
+    builder: {
+        nama: {
+            describe: 'Nama lengkap',
+            demandOption: true,
+            type: 'string',
+        },
+        email: {
+            describe: 'Email',
+            demandOption: false,
+            type: 'string',
+        }, 
+        noHP: {
+            describe: 'Nomor HP',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler(argv) {
+        contacts.simpanContact(argv.nama, argv.email, argv.noHP)
+    }
+})
 
-    contacts.simpanContact(nama, noHP, email)
-}
+yargs.parse();
 
-main();
+
+
+// const contacts = require('./contact')
+
+// const main = async () => {
+//     const nama = await contacts.pertanyaan('Masukkan nama anda : ')
+//     const noHP = await contacts.pertanyaan('Masukkan no HP anda : ')
+//     const email = await contacts.pertanyaan('Masukkan email anda : ')
+
+//     contacts.simpanContact(nama, noHP, email)
+// }
+
+// main();
